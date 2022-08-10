211ce73b24752032604da6089ea4d9d1a15dc44a
ethan_chang <ethan_chang@trendmicro.com>
2022-08-10 20:03:57 +0800

    Support child process only

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/gorilla/websocket v1.4.2
+	github.com/mitchellh/go-ps v1.0.0
 	github.com/struCoder/pidusage v0.2.1
 )
--- statsviz.go
+++ statsviz.go
@@ -2,10 +2,12 @@ package statsviz
 
 import (
 	"os"
+	"path/filepath"
 	"runtime"
 	"time"
 
 	"github.com/gorilla/websocket"
+	ps "github.com/mitchellh/go-ps"
 	"github.com/struCoder/pidusage"
 )
 
@@ -21,8 +23,39 @@ type stats struct {
 	NumGoroutine int
 }
 
-func pidUsageDuringTime(info *pidInfo) {
-	sysInfo, err := pidusage.GetStat(os.Getpid())
+// return child process if it has a parent process
+func getPid() int {
+	ppid := os.Getppid()
+	pid := os.Getpid()
+	fname := filepath.Base(os.Args[0])
+
+	parent, err := ps.FindProcess(ppid)
+	if err != nil {
+		return -1
+	}
+	if parent.Executable() == fname {
+		// it's child
+		return pid
+	}
+
+	plist, err := ps.Processes()
+	if err != nil {
+		return -1
+	}
+	// it might be a parent, looking for the child...
+	for _, p := range plist {
+		if p.Executable() == fname && p.Pid() != pid {
+			// it's child
+			return p.Pid()
+		}
+	}
+
+	// it's a single process
+	return pid
+}
+
+func pidUsageDuringTime(pid int, info *pidInfo) {
+	sysInfo, err := pidusage.GetStat(pid)
 	if err == nil {
 		info.CPUPercentage = float64(sysInfo.CPU / 100.0)
 		info.MemBytes = uint64(sysInfo.Memory)
@@ -34,11 +67,11 @@ func sendStats(conn *websocket.Conn, frequency time.Duration) error {
 	tick := time.NewTicker(frequency)
 	defer tick.Stop()
 
+	pid := getPid()
 	stats := stats{GoVersion: runtime.Version()}
 	for range tick.C {
 		runtime.ReadMemStats(&stats.Mem)
-		//sysProcessInfo(&stats.ProcessInfo)
-		pidUsageDuringTime(&stats.ProcessInfo)
+		pidUsageDuringTime(pid, &stats.ProcessInfo)
 		stats.NumGoroutine = runtime.NumGoroutine()
 		if err := conn.WriteJSON(stats); err != nil {
 			return err
